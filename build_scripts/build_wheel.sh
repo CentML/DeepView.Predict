47cfe1309273837231984249ea9a028f4b24232e
John Calderon <john.calderon@centml.ai>
2024-02-29 14:17:26 -0500

    added new env variable for pytorch version

--- build_scripts/build_wheel.sh
+++ build_scripts/build_wheel.sh
@@ -1,11 +1,13 @@
 PYTHON_VERSION=$1
-CUDA_TAG=$2
 VENV_NAME=venv_${PYTHON_VERSION}
 
 python3 -m virtualenv ${VENV_NAME} -p ${PYTHON_VERSION}
 ln -s /usr/bin/${PYTHON_VERSION}-config ${VENV_NAME}/bin/python3-config
 
 source $VENV_NAME/bin/activate
+
+pip install ${TORCH_VERSION}
+pip install numpy tqdm pandas
 
 rm -r cpp/build analyzer/habitat/*.so
 git submodule update --init --recursive
@@ -15,6 +17,8 @@ pushd analyzer
 ./install-dev.sh --build
 python3 setup.py bdist_wheel
 popd
+
+pip list
 
 deactivate
 
